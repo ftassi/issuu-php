baaff6a50815199ed4ca4d9a13dbc5b233c9b801
Francesco Tassi <tassi.francesco@gmail.com>
2011-05-19 12:37:30 +0200

    implementazione classe Abstract IssuuMessage

---
+++ lib/IssuuMessage.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of IssuuMessage
+ *
+ * @author ftassi
+ */
+abstract class IssuuMessage implements ArrayAccess
+{
+  /**
+   *
+   * @var array
+   */
+  protected $parameters;
+  
+  public function offsetExists($offset)
+  {
+    return isset($this->parameters[$offset]);
+  }
+  
+  public function offsetGet($offset)
+  {
+    return $this->offsetExists($offset) ? $this->parameters[$offset] : null;
+  }
+  
+  public function offsetSet($offset, $value)
+  {
+    if (is_null($offset))
+    {
+      throw new BadMethodCallException('Parameter Name is mandatory');
+    }
+
+    $this->parameters[$offset] = $value;
+  }
+  
+  public function offsetUnset($offset)
+  {
+    unset($this->parameters[$offset]);
+  }
+  
+  public function getParameters()
+  {
+    return $this->parameters;
+  }
+}
+
+?>
