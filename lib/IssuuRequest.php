f48ba42d838fc8a415c1b8ffb868748594d7b2f4
Francesco Tassi <tassi.francesco@gmail.com>
2011-05-19 12:38:07 +0200

    rifattorizzato IssuuRequest per estendere da IssuuMessage

--- lib/IssuuRequest.php
+++ lib/IssuuRequest.php
@@ -10,43 +10,8 @@
  *
  * @author ftassi
  */
-class IssuuRequest implements ArrayAccess
+class IssuuRequest extends IssuuMessage 
 {
-  /**
-   *
-   * @var array
-   */
-  protected $parameters;
-  
-  public function offsetExists($offset)
-  {
-    return isset($this->parameters[$offset]);
-  }
-  
-  public function offsetGet($offset)
-  {
-    return $this->offsetExists($offset) ? $this->parameters[$offset] : null;
-  }
-  
-  public function offsetSet($offset, $value)
-  {
-    if (is_null($offset))
-    {
-      throw new BadMethodCallException('Parameter Name is mandatory');
-    }
-
-    $this->parameters[$offset] = $value;
-  }
-  
-  public function offsetUnset($offset)
-  {
-    unset($this->parameters[$offset]);
-  }
-  
-  public function getParameters()
-  {
-    return $this->parameters;
-  }
 }
 
 ?>
--- test/unit/IssuuRequestTest.php
+++ test/unit/IssuuRequestTest.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once dirname(__FILE__) . '/../../lib/IssuuMessage.php';
 require_once dirname(__FILE__) . '/../../lib/IssuuRequest.php';
 
 /**
